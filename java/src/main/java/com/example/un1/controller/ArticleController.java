55ba71bd918dd015c1d4a687cdd1f1e8e15c2deb
Administrator <1784264314@qq.com>
2024-03-25 09:53:40 +0800

    123

--- java/src/main/java/com/example/un1/controller/ArticleController.java
+++ java/src/main/java/com/example/un1/controller/ArticleController.java
@@ -2,23 +2,28 @@ package com.example.un1.controller;
 
 import com.example.un1.mapper.ArticleMapper;
 import com.example.un1.pojo.Article;
+import com.example.un1.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @Slf4j
 public class ArticleController {
     @Autowired
-    ArticleMapper artileMapper;
-    @GetMapping("/ttst")
+    ArticleService articleService;
+    @GetMapping("/hello")
     public String SelectAll(){
-//        ArticleMapper artileMapper = sqlSession.getMapper(ArticleMapper.class);
-        List<Article> aritileList = artileMapper.selectAllAll();
+        List<Article> aritileList = articleService.selectAllAll();
         log.info("{}",aritileList);
-        return "yees";
+        return "/hello";
+    }
+    @GetMapping("/ano")
+    public String ano(){
+        return "/view/ano";
     }
 }
