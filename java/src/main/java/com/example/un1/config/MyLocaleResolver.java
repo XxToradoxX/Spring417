375ca51ca98a8a2741a76df4f497834a0fa4466d
Administrator <1784264314@qq.com>
2024-03-28 14:45:23 +0800

    03281445

---
+++ java/src/main/java/com/example/un1/config/MyLocaleResolver.java
@@ -0,0 +1,39 @@
+package com.example.un1.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+@Configuration
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String s = request.getParameter("l");
+        String header = request.getHeader("Accept-Language");
+        System.out.println(header);
+        Locale locale = null;
+        if (!StringUtils.isEmpty(s)) {
+            String[] split = s.split("_");
+            locale = new Locale(split[0], split[1]);
+        } else {
+            String[] splits = header.split(",");
+            String[] split = splits[0].split("-");
+            locale = new Locale(split[0], split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+    }
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+}
--- java/src/main/java/com/example/un1/controller/LoginController.java
+++ java/src/main/java/com/example/un1/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.un1.controller;
 
+import com.example.un1.config.MyLocaleResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
